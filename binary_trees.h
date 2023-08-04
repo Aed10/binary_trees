f26d0230f83cb7153489bf0e6bb5c58ada7494c1
Aed10 <eddyyanyayoub@gmail.com>
2023-08-04 13:34:25 +0100

    Create initial files and directories for binary tree project

---
+++ binary_trees.h
@@ -0,0 +1,38 @@
+#ifndef BIN_TREES_H
+# define BIN_TREES_H
+
+/* Libraries */
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+/* Structures */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+/* Binary tree */
+typedef struct binary_tree_s
+{
+	int	n;
+	struct binary_tree_s	*parent;
+	struct binary_tree_s	*left;
+	struct binary_tree_s	*right;
+
+}	binary_tree_t;
+
+/* AVL tree */
+typedef struct binary_tree_s	avl_t;
+
+/* Prototypes */
+/* Mandatory tasks */
+binary_tree_t *binary_tree_node(binary_tree_t *parent,
+									int value);
+
+#endif /* BIN_TREES_H */
